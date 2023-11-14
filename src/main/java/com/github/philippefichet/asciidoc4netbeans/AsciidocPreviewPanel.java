ef1182313271738dd5d7e22d93f20d3915901dc4
FICHET Philippe <philippe.fichet@laposte.net>
2023-11-14 17:44:00 +0100

    Ignore java:S4968 on JavaFX API

--- src/main/java/com/github/philippefichet/asciidoc4netbeans/AsciidocPreviewPanel.java
+++ src/main/java/com/github/philippefichet/asciidoc4netbeans/AsciidocPreviewPanel.java
@@ -69,6 +69,9 @@ public class AsciidocPreviewPanel extends javax.swing.JPanel {
      * Creates new form AsciidocPreviewPanel
      * @param xrefHandler handler for click on XRef/hyperlink
      */
+    @SuppressWarnings({
+        "java:S4968" // Replace this type parametrization by the 'final' type `String`. Disable, not applicable on JavaFX API
+    })
     public AsciidocPreviewPanel(Consumer<File> xrefHandler) {
         initComponents();
         this.xrefHandler = xrefHandler;
